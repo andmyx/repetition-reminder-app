aeb66307abbfe9052c4b41514d5083032f74bdd3
Zelmyx <kryss10an@gmail.com>
2020-06-16 18:53:40 +0200

    change header height

--- src/navigation/stacknavigation/stackheaderstyle.js
+++ src/navigation/stacknavigation/stackheaderstyle.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 export default StackHeaderStyles = {
   headerStyle: {
     backgroundColor: "#f0a0a0",
+    height: 80,
   },
   headerTintColor: "#333333",
   headerTitleStyle: {
