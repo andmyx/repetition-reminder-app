546b971b66a2ef9a7fdcfb9585c4a46b02902a53
Zelmyx <kryss10an@gmail.com>
2020-06-21 12:50:43 +0200

    add basic ability to show and create reminders
    This commit does not involve a database nor the ability to inspect the created reminders.

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,16 +1,50 @@
-import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from "react-native";
 
-export default function Home({ navigation }) {
+export default function Home({ route, navigation }) {
+  const [reminders, setReminders] = useState([
+    { subject: "subject1", title: "title1", body: "body1", key: "1" },
+    { subject: "subject2", title: "title2", body: "body2", key: "2" },
+    { subject: "subject3", title: "title3", body: "body3", key: "3" },
+  ]);
+
+  React.useEffect(() => {
+    if (route.params?.values) {
+      addReview(route.params.values);
+    }
+  }, [route.params?.values]);
+
+  function addReview(values) {
+    values.key = Math.random().toString();
+    setReminders((currentReminders) => {
+      return [values, ...currentReminders];
+    })
+  }
 
   function goToReminderCreate() {
-    navigation.push("ReminderCreate")
+    navigation.push("ReminderCreate");
+  }
+
+  function flatListItem(title) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
       <Text>This is the homescreen</Text>
       <Button title="Create a reminder" onPress={goToReminderCreate} />
+      <FlatList
+        data={reminders}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            {flatListItem(item.title)}
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
@@ -21,4 +55,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#00f000",
     padding: 30,
   },
+  item: {
+    backgroundColor: "#ffffff",
+    marginTop: 20,
+    padding: 5,
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#ff0000",
+  }
 });
--- src/screens/reminderCreate.js
+++ src/screens/reminderCreate.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { Formik } from "formik";
 
-export default function ReminderCreate() {
+export default function ReminderCreate({ route, navigation }) {
+
   return (
     <View style={styles.container}>
       <Text>This is the reminder-create screen</Text>
+      <Formik
+        initialValues={{ subject: "", title: "", body: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+          navigation.navigate("Home", { values: values });
+        }}
+      >
+        {(formikProps) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="Subject"
+              onChangeText={formikProps.handleChange("subject")}
+              value={formikProps.values.subject}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Title"
+              onChangeText={formikProps.handleChange("title")}
+              value={formikProps.values.title}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Body"
+              onChangeText={formikProps.handleChange("body")}
+              value={formikProps.values.body}
+              multiline
+            />
+
+            <Button title="Submit" onPress={formikProps.handleSubmit} />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -12,7 +47,15 @@ export default function ReminderCreate() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ff00ff",
-    padding: 30,
+    backgroundColor: "#ffffff",
+    padding: 15,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 10,
+    marginTop: 10,
+  }
 });
