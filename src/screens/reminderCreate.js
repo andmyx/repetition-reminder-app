1feaf24c733b77f2780f9eb41380e28baa187140
Zelmyx <kryss10an@gmail.com>
2020-06-21 13:21:16 +0200

    add ability to dismiss keyboard

--- src/screens/reminderCreate.js
+++ src/screens/reminderCreate.js
@@ -1,46 +1,48 @@
 import React from "react";
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button, Keyboard, TouchableWithoutFeedback } from "react-native";
 import { Formik } from "formik";
 
 export default function ReminderCreate({ route, navigation }) {
 
   return (
-    <View style={styles.container}>
-      <Text>This is the reminder-create screen</Text>
-      <Formik
-        initialValues={{ subject: "", title: "", body: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-          navigation.navigate("Home", { values: values });
-        }}
-      >
-        {(formikProps) => (
-          <View>
-            <TextInput
-              style={styles.input}
-              placeholder="Subject"
-              onChangeText={formikProps.handleChange("subject")}
-              value={formikProps.values.subject}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Title"
-              onChangeText={formikProps.handleChange("title")}
-              value={formikProps.values.title}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Body"
-              onChangeText={formikProps.handleChange("body")}
-              value={formikProps.values.body}
-              multiline
-            />
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text>This is the reminder-create screen</Text>
+        <Formik
+          initialValues={{ subject: "", title: "", body: "" }}
+          onSubmit={(values) => {
+            console.log(values);
+            navigation.navigate("Home", { values: values });
+          }}
+        >
+          {(formikProps) => (
+            <View>
+              <TextInput
+                style={styles.input}
+                placeholder="Subject"
+                onChangeText={formikProps.handleChange("subject")}
+                value={formikProps.values.subject}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Title"
+                onChangeText={formikProps.handleChange("title")}
+                value={formikProps.values.title}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Body"
+                onChangeText={formikProps.handleChange("body")}
+                value={formikProps.values.body}
+                multiline
+              />
 
-            <Button title="Submit" onPress={formikProps.handleSubmit} />
-          </View>
-        )}
-      </Formik>
-    </View>
+              <Button title="Submit" onPress={formikProps.handleSubmit} />
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
